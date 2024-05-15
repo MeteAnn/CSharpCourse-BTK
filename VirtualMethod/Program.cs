48297e4ffbfc5c04e919e68627492265b4c9d4b9
MeteAnn <meeteann@gmail.com>
2024-05-15 11:26:37 +0300

    İnheritance, VirtualMethod

---
+++ VirtualMethod/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            SqlServer sqlserver = new SqlServer();
+            sqlserver.Add();
+
+            MySql mysql = new MySql();
+            mysql.Delete();
+
+            Console.ReadLine();
+
+        }
+        
+    }
+
+    class Database
+    {
+
+
+        public virtual void Add()
+        {
+
+            Console.WriteLine("Added by default");
+
+        }
+
+        public virtual void Delete()
+        {
+
+            Console.WriteLine("Deleted by Default");
+
+        }
+
+    }
+    class SqlServer:Database
+    {
+
+        public override void Add()
+        {
+
+
+            Console.WriteLine("Added by Sql Code");
+
+            //base.Add(); //Burada base demek temeli yani SqlServerin temeli kim Database yani buna base deniyor basein add ni çalıştırıyor.
+        }
+
+
+    }
+
+    class MySql:Database
+    {
+
+
+
+    }
+
+
+}
