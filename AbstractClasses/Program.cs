c789f02e82c54ecd5165b11b2556f12cbb811fab
MeteAnn <meeteann@gmail.com>
2024-05-15 13:30:03 +0300

    AbstractClass

---
+++ AbstractClasses/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Database database = new Oracle();
+            database.Delete();
+            database.Add();
+
+            Database database2 = new SqlServer();
+            database2.Delete();
+            database2.Add();
+
+        }
+
+
+        abstract class Database
+        {
+
+
+            public void Add()
+            {
+
+                Console.WriteLine("Added by default");
+            }
+
+            public abstract void Delete();
+        }
+
+        class SqlServer : Database
+        {
+            public override void Delete()
+            {
+                Console.WriteLine("Deleted by S");
+            }
+        }
+
+        class Oracle : Database
+        {
+            public override void Delete()
+            {
+                Console.WriteLine("Deleted by Oracle");
+            }
+
+        }
+
+
+    }
+}
