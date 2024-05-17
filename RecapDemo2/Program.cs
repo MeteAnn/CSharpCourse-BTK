e90cd7e9f3f445fdca89f483fd7feb3f66052452
MeteAnn <meeteann@gmail.com>
2024-05-17 10:57:13 +0300

    RecapDemo2

---
+++ RecapDemo2/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecapDemo2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CustomerManager customerManager = new CustomerManager();
+            
+
+            customerManager.Add();
+            Console.WriteLine("Customer added!");
+
+
+
+        }
+    }
+
+    class CustomerManager
+    {
+        public ILogger logger { get; set; }
+
+        public void Add()
+        {
+
+            logger.Log();
+            Console.WriteLine("Customer Added!");
+        }
+
+    }
+    class DatabaseLogger:ILogger
+    {
+
+
+        public void Log()
+        {
+
+
+            Console.WriteLine("Logged To Database!");
+
+
+        }
+
+    }
+
+    class FileLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged to file!");
+        }
+    }
+
+    class SmsLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged to Sms!");
+        }
+    }
+
+    interface ILogger
+    {
+
+
+        void Log();
+
+
+    }
+
+}
